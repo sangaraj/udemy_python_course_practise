03576bad1841e3d298a14990ae27ae86d046b552
Rohit Sangaraj <rohitsangaraj@Rohits-MacBook-Pro.local>
2021-05-29 19:53:01 +0530

    added hello world program

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+#first comment in python
+print("Hello Python")
+print(' this is my first program')
+
+'''
+multi line comment in python
+using single quotes
+'''
+
+"""
+multin line comment in python 
+using double quotes
+"""
